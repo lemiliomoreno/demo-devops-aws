f2739d0b818fe17b052d138306524050f22c4aa2
Luis E Moreno <lemiliomoreno@gmail.com>
2024-05-07 09:15:12 -0600

    fix ci

--- src/utils/validate_token.py
+++ src/utils/validate_token.py
@@ -8,7 +8,7 @@ from utils.exceptions import Unauthorized
 
 async def validate_token(x_token: Annotated[str, Header()]):
     database = await get_db()
-    
+
     token_exists = await database.users.find_one({"token": x_token})
 
     if not token_exists:
