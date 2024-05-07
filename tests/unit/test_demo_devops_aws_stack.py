149bcdfaeb98ce8999d585497e6e4f3a7ec1fbef
Luis E Moreno <lemiliomoreno@gmail.com>
2024-05-07 10:26:54 -0600

    fix: assign public ip to fargate service

--- tests/unit/test_demo_devops_aws_stack.py
+++ tests/unit/test_demo_devops_aws_stack.py
@@ -3,12 +3,14 @@ import aws_cdk.assertions as assertions
 
 from demo_devops_aws.demo_devops_aws_stack import DemoDevopsAwsStack
 
+
 # example tests. To run these tests, uncomment this file along with the example
 # resource in demo_devops_aws/demo_devops_aws_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = DemoDevopsAwsStack(app, "demo-devops-aws")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
